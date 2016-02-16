85cd560e609d87eb3782c93bb587d3b97bd6e8d6
Carter Bray <carter.bray@intel.com>
2016-02-15 21:44:28 -0800

    Move CMake generation scripts to cmake subdirectory

--- cmake/cmake_visual_studio_2015_x64.bat
+++ cmake/cmake_visual_studio_2015_x64.bat
@@ -1,8 +1,8 @@
 @echo off
 
 cmake -G "Visual Studio 14 2015 Win64"  %"Set the generator to Visual Studio 2015 x64"% ^
-      -H.                               %"Set the source directory to the current directory"% ^
-      -B.\_build                        %"Set the build directory to the _build subdirectory"% ^
+      -H%~dp0\..                        %"Set the source directory to the parent directory"% ^
+      -B%~dp0\..\_build                 %"Set the build directory to the _build subdirectory"% ^
       %*                                %"Forward additional command line arguments to CMake"%
 
 @echo on
