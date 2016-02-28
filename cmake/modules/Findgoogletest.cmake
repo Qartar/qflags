93c6ada0b57552cdbf8a9ca0a5867d8419244067
Carter Bray <carter.bray@intel.com>
2016-02-28 15:11:55 -0800

    Move googletest inclusion into cmake module

---
+++ cmake/modules/Findgoogletest.cmake
@@ -0,0 +1,18 @@
+# Findgoogletest
+
+# Set the default path to the googletest submodule
+set(GOOGLETEST_PATH "${CMAKE_SOURCE_DIR}/googletest")
+
+# Allow qflags to be built without Google Test
+if(EXISTS ${GOOGLETEST_PATH}/CMakeLists.txt)
+    add_subdirectory(${GOOGLETEST_PATH})
+
+    # If the toolchain was set to use Microsoft's Clang/C2 compilers then force
+    # gmock and gtest to use the normal C1/C2 compiler because otherwise all of
+    # their Microsoft specific configuration would need to be redone.
+    if(MSVC AND ${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+        foreach(target gtest gtest_main gmock gmock_main)
+            set_target_properties(${target} PROPERTIES VS_PLATFORM_TOOLSET_OVERRIDE "v140")
+        endforeach()
+    endif()
+endif()
