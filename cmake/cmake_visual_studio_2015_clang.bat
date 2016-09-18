ce87ad9bdb76b48857b21dfdb171d12c1871549a
Carter Bray <carter.bray@intel.com>
2016-09-17 20:48:12 -0700

    Update Clang/C2 batch file with new toolchain name

--- cmake/cmake_visual_studio_2015_clang.bat
+++ cmake/cmake_visual_studio_2015_clang.bat
@@ -1,7 +1,7 @@
 @echo off
 
 cmake -G "Visual Studio 14 2015 Win64"  %"Set the generator to Visual Studio 2015 x64"% ^
-      -T "v140_clang_3_7"               %"Set the compiler toolchain to Clang/C2"% ^
+      -T "v140_clang_c2"                %"Set the compiler toolchain to Clang/C2"% ^
       -H%~dp0\..                        %"Set the source directory to the parent directory"% ^
       -B%~dp0\..\_build_clang           %"Set the build directory to the _build_clang subdirectory"% ^
       %*                                %"Forward additional command line arguments to CMake"%
